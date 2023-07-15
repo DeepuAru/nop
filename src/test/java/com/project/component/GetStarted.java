d2938d60b01191e35c17bbe3d17c058207489f2c
aravi <aravi@DESKTOP-M73D63C>
2023-07-15 09:50:31 +0530

    GetStarted_Naresh Anna update

--- src/test/java/com/project/component/GetStarted.java
+++ src/test/java/com/project/component/GetStarted.java
@@ -2,6 +2,8 @@ package com.project.component;
 
 import static com.project.services.XPathConstants.*;
 
+import java.util.ArrayList;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
@@ -17,11 +19,25 @@ public class GetStarted extends LaunchSite {
 		chromeBrowser.findElement(By.xpath(Premium_Support_Services)).click();
 		chromeBrowser.navigate().back();
 		chromeBrowser.findElement(By.xpath(Documentation)).click();
-
 		
+		ArrayList<String> newTb = new ArrayList<String>(chromeBrowser.getWindowHandles());
+		chromeBrowser.switchTo().window(newTb.get(1));
+		System.out.println("Page title of new tab: " + chromeBrowser.getTitle());
+		clicking("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[3]/a[1]", 1);
+		clicking("//a[@title='Getting started']",2);
 		
-		
-		
+	}
+	
+	public void clicking(String xPath, int number) {
+		try {
+			System.out.println(number);
+			myUtil.justWait(xPath,  30);
+			chromeBrowser.findElement(By.xpath(xPath)).click();
+			
+		} catch (Exception exp) {
+			
+			exp.printStackTrace();
+		}
 	}
 
 }
